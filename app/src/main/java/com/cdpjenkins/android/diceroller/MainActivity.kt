79212f0063721f9dfc40845124f259cbb53dc8d2
Chris Jenkins <cdpjenkins@gmail.com>
2020-06-06 16:05:50 +0100

    Add dice images

--- app/src/main/java/com/cdpjenkins/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/cdpjenkins/android/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.cdpjenkins.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
@@ -24,5 +25,19 @@ class MainActivity : AppCompatActivity() {
 
         val value = Random.nextInt(6) + 1
         resultText.text = "$value"
+
+
+        val drawableResource = when (value) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_6
+        }
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
