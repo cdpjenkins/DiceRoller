4bb6ba83658a12edf69eeb85a70e15d8e87dc4dc
Chris Jenkins <cdpjenkins@gmail.com>
2020-06-06 15:39:50 +0100

    Add onClick listener to the button

--- app/src/main/java/com/cdpjenkins/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/cdpjenkins/android/diceroller/MainActivity.kt
@@ -2,11 +2,19 @@ package com.cdpjenkins.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val rollButton = findViewById<Button>(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
